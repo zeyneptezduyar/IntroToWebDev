f919f894963091fe44907e9ff3d1bd873b4990c5
Zeynep Tezduyar <zeynep@tezduyar.com>
2020-08-16 12:19:44 +0300

    modified js practice about arrays

--- javascript/practice/05-arrays/your-code.js
+++ javascript/practice/05-arrays/your-code.js
@@ -9,20 +9,30 @@
 // Tip: Start with 2 numbers in the array. Once the function works with 2 numbers, add a third, and so on.
 // Write your code here:
 
+function findMax(numbers) {
+    var max = numbers[0];
+    for (var i = 1; i <= numbers.length; i++) {
+        if(numbers[i] > max){
+            max = numbers[i]
+        }
+    }
+    return max;
+}
+
 
 // Open index.html in your browser, open the JavaScript console, confirm the result.
 
 // Uncomment the code below to verify your function is working.
-// console.log("findMax =====");
-// var numbers = [46, 3, 40, 74, 63];
-// var actual = findMax(numbers);
-// console.log("[46, 3, 40, 74, 63] -> expected: 74, actual: %s", actual);
-// numbers = [3, 3, 3, 3];
-// actual = findMax(numbers);
-// console.log("[3, 3, 3, 3] -> expected: 3, actual: %s", actual);
-// numbers = [-1];
-// actual = findMax(numbers);
-// console.log("[-1] -> expected: -1, actual: %s", actual);
+console.log("findMax =====");
+var numbers = [46, 3, 40, 74, 63];
+var actual = findMax(numbers);
+console.log("[46, 3, 40, 74, 63] -> expected: 74, actual: %s", actual);
+numbers = [3, 3, 3, 3];
+actual = findMax(numbers);
+console.log("[3, 3, 3, 3] -> expected: 3, actual: %s", actual);
+numbers = [-1];
+actual = findMax(numbers);
+console.log("[-1] -> expected: -1, actual: %s", actual);
 
 
 // 2. COMBINE ARRAYS
@@ -32,16 +42,20 @@
 // The function should NOT alter either parameter.
 // Write your code here:
 
+function combine(array1, array2) {
+    var newArray = array1 + array2;
+    return newArray;
+}
 
 
 // Uncomment the code below to verify your function is working.
-// console.log("combine =====");
-// var merged = combine(["Oak", "Maple", "Birch"], ["Hydrogen", "Helium"]);
-// console.log("expected: Oak,Maple,Birch,Hydrogen,Helium\nactual: %s", merged);
-// merged = combine([12.5, 3, 855], [true, false, true]);
-// console.log("expected: 12.5,3,855,true,false,true\nactual: %s", merged);
-// merged = combine(["Saturn"], ["Black Lab", "Borzoi", "Pug"]);
-// console.log("expected: Saturn,Black Lab,Borzoi,Pug\nactual: %s", merged);
+console.log("combine =====");
+var merged = combine(["Oak", "Maple", "Birch"], ["Hydrogen", "Helium"]);
+console.log("expected: Oak,Maple,Birch,Hydrogen,Helium\nactual: %s", merged);
+merged = combine([12.5, 3, 855], [true, false, true]);
+console.log("expected: 12.5,3,855,true,false,true\nactual: %s", merged);
+merged = combine(["Saturn"], ["Black Lab", "Borzoi", "Pug"]);
+console.log("expected: Saturn,Black Lab,Borzoi,Pug\nactual: %s", merged);
 
 
 // 3. CHALLENGE: SUBTRACT AN ARRAY
@@ -53,16 +67,33 @@
 // The function should NOT alter either parameter.
 // Write your code here:
 
+function subtractArray(array1, array2) {
+    var newArray = [];
+    var flag = false;
+    for (var i = 0; i <= array1.length; i++){
+        for (var j = 0; j <= array2.length; j++){
+            if (array1[i] == array2[j]){
+                flag = true;
+                break
+            }
+        }
+        if(flag == false) {
+            newArray[newArray.length] = array1[i];
+        }
+        flag = false;
+    }
+    return newArray;
+}
 
 
 // Uncomment the code below to verify your function is working.
-// console.log("subtractArray =====");
-// var result = subtractArray([1, 2, 3, 4, 5], [2, 4, 6]);
-// console.log("expected: 1,3,5\nactual: %s", result);
-// result = subtractArray(["Neptune", "Uranus", "Saturn", "Pluto"], ["Pluto"]);
-// console.log("expected: Neptune,Uranus,Saturn\nactual: %s", result);
-// result = subtractArray([1, 2, 3, 1, 2, 3, 1, 2, 3], [1, 3]);
-// console.log("expected: 2,2,2\nactual: %s", result);
+console.log("subtractArray =====");
+var result = subtractArray([1, 2, 3, 4, 5], [2, 4, 6]);
+console.log("expected: 1,3,5\nactual: " + result);
+result = subtractArray(["Neptune", "Uranus", "Saturn", "Pluto"], ["Pluto"]);
+console.log("expected: Neptune,Uranus,Saturn\nactual: " + result);
+result = subtractArray([1, 2, 3, 1, 2, 3, 1, 2, 3], [1, 3]);
+console.log("expected: 2,2,2\nactual: " + result);
 
 
 
